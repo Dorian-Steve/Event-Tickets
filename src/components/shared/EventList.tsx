9383d7c6312ead7d7287205b72200c579f00d65f
Dorian-Steve <doriansteve3@gmail.com>
2025-02-27 15:39:42 +0100

    Event page added

--- src/components/shared/EventList.tsx
+++ src/components/shared/EventList.tsx
@@ -10,21 +10,17 @@ import EventCard from '@/components/shared/EventCard';
 const EventList = () => {
   const events = useQuery(api.event.get);
 
-  if (!events) {
-    return (
-      <div className="min-h-[400px] flex items-center justify-center">
-        <Spinner />
-      </div>
-    );
-  }
-
   const upcomingEvents = events
-  .filter((event) => event.eventDate > Date.now())
-  .sort((a,b) => a.eventDate - b.eventDate);
+  ? events
+    .filter((event) => event.eventDate > Date.now())
+    .sort((a,b) => a.eventDate - b.eventDate)
+  : [];
 
   const pastEvents = events
-  .filter((event) => event.eventDate <= Date.now())
-  .sort((a,b) => b.eventDate - a.eventDate);
+  ? events
+    .filter((event) => event.eventDate <= Date.now())
+    .sort((a,b) => b.eventDate - a.eventDate)
+  : [];
 
   return (
     <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-4 py-2'>
@@ -32,9 +28,7 @@ const EventList = () => {
        {/* Header section */}
       <div className="flex item-center justify-between md-8">
         <div>
-          <h1 className="text-xl font-bold text-gray-900">
-            Upcoming Events
-          </h1>
+          <h1 className="text-xl font-bold text-gray-900">Upcoming Events</h1>
           <p className="mt-2 text-gray-600">
             Discover & Book tickets for amazing events
           </p>
@@ -50,6 +44,12 @@ const EventList = () => {
       </div>
 
       {/* Upcoming Events */}
+      {!events ? (
+        <div className="min-h-[400px] flex items-center justify-center">
+          <Spinner />
+        </div>
+      ) : (
+        <>
       {upcomingEvents.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md-12">
           {upcomingEvents.map((event) => (
@@ -62,9 +62,9 @@ const EventList = () => {
               <h3 className="text-lg font-medium text-gray-900">
                 No Upcoming Event
               </h3>
-              <p className="text-gray-600 mt-1">Check back kater for new events</p>
+              <p className="text-gray-600 mt-1">Check back latter for new events</p>
           </div>
-          )},
+          )}
 
           {/* Past Events */}
       {pastEvents.length > 0 && (
@@ -77,7 +77,8 @@ const EventList = () => {
           </div>
         </>
         )}
-        
+        </>
+        )}
     </div>
   )
 }
